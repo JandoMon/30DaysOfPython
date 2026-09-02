4a5fd276f2a6b0f48c64284ec5a8c03ef4c9c6ba
JandoMon <166460188+JandoMon@users.noreply.github.com>
2026-09-01 21:39:51 -0500

    Add files via upload
    Tracking my progress by revisiting a language I have learned in the past. Day 1 Complete! :)

---
+++ 30DaysOfPython/day_1/exercise3.py
@@ -0,0 +1,23 @@
+#Integer
+print(type(1))
+#Float
+print(type(1.5))
+#Complex
+print(type(1+5j))
+#String
+print(type("Hello World!"))
+#Boolean
+print(type(True))
+#List
+print(type([4,5,6,3,1]))
+#Tuple
+print(type((1,2,3,5)))
+#Set
+print(type({1,2,5,6}))
+#Dictionary
+print(type({'sad':'A feeling that someone has when things are not going there way'}))
+
+#Euclidean Distance is the straight line length between two points in a space calculated using pythagorean theorem
+# sqr((y_2 - y_1)^2 + (x_2 - x_1)^2)
+# sqr((25) + (64))
+# 89 --> sqr(89)
---
+++ 30DaysOfPython/day_1/helloworld.py
@@ -0,0 +1,18 @@
+#Day 1- 30DaysofPython Challenge
+print(2+3)      #addition(+)
+print(3-1)      #subtraction(-)
+print(2*3)      #mulitpliaction(*)
+print(3/2)      #division(/)
+print(3**2)     #exponential(**)
+print(3%2)      #modulus(%)
+print(3//2)     #floor(//)
+
+#checking data types
+print(type(10))         #Int
+print(type(3.14))       #Float
+print(type(1+3j))       #Complex Number
+print(type('Asabenth')) #String
+print(type([1,2,3]))    #List
+print(type({'name':'Asabenth'}))    #Dictionary
+print(type({9.8,3.14,2.7}))         #Set
+print(type((9.8,3.14,2.7)))         #Tuple
