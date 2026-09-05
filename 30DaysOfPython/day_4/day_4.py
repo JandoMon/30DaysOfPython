90ee1043b64c53f04006ac5cf85a532e03b0cb5f
JandoMon <166460188+JandoMon@users.noreply.github.com>
2026-09-04 23:07:32 -0500

    Add files via upload
    Day 3: partially complete... busy at work... will finish in the morning... along with day 4 ._.

---
+++ 30DaysOfPython/day_4/day_4.py
@@ -0,0 +1,33 @@
+#Q1: Concatenating 
+thirty = 'Thirty'
+days = 'Days'
+of = 'Of'
+python ="Python"
+space = ' '
+python_30 = thirty + space + days + space + of + space + python
+print(python_30)
+
+#Q2: Concatenating
+coding = 'Coding'
+for_ = 'For'
+all_ = 'All'
+space = ' '
+coding_for_all = coding + space + for_ + space + all_
+print(coding_for_all)
+
+#Q3: string variable
+company = "Coding for All"
+
+#Q4: print variable
+print(company)
+
+#Q5: print len
+print(len(company))
+
+#Q6: uppercase
+print(company.upper())
+
+#Q7: lowercase
+print(company.lower())
+
+
