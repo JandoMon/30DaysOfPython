4ab5f44a74d465709e247e635a153c4b186860f4
JandoMon <166460188+JandoMon@users.noreply.github.com>
2026-09-05 23:36:18 -0500

    Add files via upload
    Day 3: I was at a birthday party... i will work in the morning tmr :)

--- 30DaysOfPython/day_4/day_4.py
+++ 30DaysOfPython/day_4/day_4.py
@@ -30,4 +30,28 @@ print(company.upper())
 #Q7: lowercase
 print(company.lower())
 
+#Q8: capitalize, title, swapcase
+coding_for_all = "Coding For All"
+print(coding_for_all.capitalize())
+print(coding_for_all.title())
+print(coding_for_all.swapcase())
+
+#Q9: slice the for word in Coding For All
+print(coding_for_all[0:6])
+
+#Q10: Check to see if Coding is in the word
+print(coding_for_all.index('Coding'))
+print(coding_for_all.find('Coding'))
+
+#Q11: Replace the word Coding for Python
+print(coding_for_all.replace('Coding', 'Python'))
+
+#Q12: Replace the word to another 
+print(coding_for_all.replace('Coding', 'Python').replace('All', 'Everyone'))
+
+#Q13: Split Coding for All using space as the seperator 
+print(coding_for_all.split(' '))
+
+
+
 
