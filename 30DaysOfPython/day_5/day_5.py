1c69e14ef96825958e6dc490db3cfb041a1c98dd
JandoMon <166460188+JandoMon@users.noreply.github.com>
2026-09-08 21:00:27 -0500

    Add files via upload
    Day 5 halfway done... I am still trying to catch up, but this one is much easier and much shorter... good progress today :)

---
+++ 30DaysOfPython/day_5/day_5.py
@@ -0,0 +1,51 @@
+#Q1: Declare an empty list
+print('Q1: Declare an empty list')
+empty_lst = []
+print(empty_lst)
+print()
+
+#Q2: Declare a list with 5 items
+print('Q2: List with 5 items')
+five_item_lst = [1,2,3,4,5]
+print(five_item_lst)
+print()
+
+#Q3: Find the length of the list
+print('Q3: Length of List')
+print(len(five_item_lst))
+print()
+
+#Q4: Get the first item, middle item, last item
+print('Q4: First item, Middle item, Last item')
+print(five_item_lst[0], five_item_lst[int((len(five_item_lst)-1)/2)], five_item_lst[len(five_item_lst)-1])
+print()
+
+#Q5: Declare a list of mixed data types
+print('Q5: Declare a list of mixed data types')
+print(['Paul', 25, '6ft', 'single', '12603 wouldnt you like to know'])
+print()
+
+#Q6: Declare list
+print('Q6: Declare a list of IT companies')
+it_companies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+print()
+
+#Q7: print the list
+print('Q7: Print the list')
+print(it_companies)
+print()
+
+#Q8: number of companies in the list
+print('Q8: Num of companies in the list')
+print(len(it_companies))
+
+#Q9: First, Middle, Last Company
+print('Q9: First, Middle, Last Company')
+print(it_companies[0], it_companies[int((len(it_companies)-1)/2)], it_companies[len(it_companies)-1])
+print()
+
+#Q10: Print the list after modifying the list
+print('Q10: Print the list after modifying the list')
+it_companies[0]= 'X'
+print(it_companies)
+print()
