e6e859af2ab2083c98581e83d416ee1dd79d1bea
JandoMon <166460188+JandoMon@users.noreply.github.com>
2026-09-12 11:52:52 -0500

    Add files via upload
    Day 6: COMPLETE !!! Nothing like morning coding to wake the bones up

--- 30DaysOfPython/day_6/day_6.py
+++ 30DaysOfPython/day_6/day_6.py
@@ -39,3 +39,52 @@ print(siblings)
 print(mother)
 print(father)
 print()
+
+#Q2: Create a tuple for fruits, vegetable, and animal products
+print('Create a tuple for fruits, vegetables, and animal products')
+fruits = 'banana', 'apple', 'mango', 'strawberry'
+vegetables = 'onion', 'lettuce', 'tomato', 'cucumber'
+animal_products = 'milk', 'honey', 'cheese', 'beef'
+print(fruits)
+print(vegetables)
+print(animal_products)
+food_stuff_tp = fruits + vegetables + animal_products
+print(food_stuff_tp)
+print()
+
+#Q3: Change the tuple to a list
+print('Change the tuple to a list')
+food_stuff_lt = list(food_stuff_tp)
+print(food_stuff_lt)
+print()
+
+#Q4: Slice the middle of the list
+print('Slice out the middle of the list')
+middle_item_index = int((len(food_stuff_lt)-1)/2)
+middle_item = food_stuff_lt[middle_item_index]
+print(middle_item)
+print()
+
+#Q5: Slice first three and last three
+print('Slice out the first three and last three')
+first_three = food_stuff_lt[0:3]
+last_three = food_stuff_lt[-4:-1]
+print(first_three)
+print(last_three)
+print()
+
+#Q6: Delete food tuple
+print('Delete food tuple')
+del food_stuff_tp
+try:
+    print(food_stuff_tp)
+except NameError:
+    print("Success: The variable 'x' no longer exists.")
+print()
+
+#Q7: Check values in tuple
+print('Check values in tuple')
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+print('Estonia' in nordic_countries)
+print('Iceland' in nordic_countries)
+print()
