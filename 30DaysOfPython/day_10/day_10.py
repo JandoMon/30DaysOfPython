c1a6c3c5e0e1ea34366a08585da1c2e8217593ca
JandoMon <166460188+JandoMon@users.noreply.github.com>
2026-09-20 02:29:31 -0500

    Add files via upload
    Day 10: Late-night coding... after having a soccer tournament all day... I am exhausted but still determined to spend at least 30 mins

---
+++ 30DaysOfPython/day_10/day_10.py
@@ -0,0 +1,11 @@
+#Q1: Ask for age and see if they can drive
+print('Ask for age and see if they can drive')
+age = input('Enter your age:')
+age = int(age)
+if age > 18: 
+    print('You are old enough to drive')
+else:
+    print('You need', 18 - age, 'more years to drive.')
+print()
+
+#Q2: 
