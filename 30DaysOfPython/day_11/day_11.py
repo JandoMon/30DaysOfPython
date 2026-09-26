2c0f553a15ffb2e218fcf48f15a3d45262de5062
JandoMon <166460188+JandoMon@users.noreply.github.com>
2026-09-26 00:19:36 -0500

    Add files via upload
    Day 11: I am sick...

--- 30DaysOfPython/day_11/day_11.py
+++ 30DaysOfPython/day_11/day_11.py
@@ -1,1 +1,5 @@
-#LETS GET IT!
+#Q1: Declare a function add_two_numbers. It takes two parameters and it returns a sum
+print('Declare a function add_two_numbers. It takes two parameters and it returns a sum')
+def add_two_numbers(num_one, num_two):
+    return  num_one + num_two
+print(add_two_numbers(5,3))
